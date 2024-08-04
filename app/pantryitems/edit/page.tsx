652b44b73707953cf2884eb49eb19289ac4b63a6
basupatil1213 <patil.ba@northeastern.edu>
2024-08-04 12:21:36 -0400

    bug fix pantry items route

--- app/pantryitems/edit/page.tsx
+++ app/pantryitems/edit/page.tsx
@@ -1,20 +1,26 @@
 'use client';
 import PantryItemInputForm from "@/components/Form/PantryItemInputForm";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-const EditPantryItem = () => {
+const EditPantryItemContent = () => {
     const searchParams = useSearchParams();
     const item = searchParams.get('item');
     console.log(`Item: ${item}`);
-    
 
     const parsedItem = item ? JSON.parse(item) : undefined;
 
     console.log(`Parsed item: ${JSON.stringify(parsedItem)}`);
-    
 
     return <PantryItemInputForm action="edit" item={parsedItem}/>;
+};
+
+const EditPantryItem = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <EditPantryItemContent />
+        </Suspense>
+    );
 };
 
 export default EditPantryItem;
